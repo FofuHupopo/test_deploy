27814cdef2958c3026a6bd8ff0a21f0f123c98c4
sadside <75984650+sadside@users.noreply.github.com>
2022-06-12 17:09:29 +0500

    fff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link} from "react-router-dom";
 
 function App() {
   return (
@@ -9,14 +10,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <Link
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          to="/second"
         >
           Learn React
-        </a>
+        </Link>
       </header>
     </div>
   );
